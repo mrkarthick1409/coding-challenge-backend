259751853e68eecbbc27884193391768e2a33922
Karthick <karthick.manickam@nium.com>
2022-06-08 18:41:48 +0530

    initial commit

---
+++ src/main/java/com/am/appreview/AppReviewApplication.java
@@ -0,0 +1,13 @@
+package com.am.appreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReviewApplication.class, args);
+	}
+
+}
